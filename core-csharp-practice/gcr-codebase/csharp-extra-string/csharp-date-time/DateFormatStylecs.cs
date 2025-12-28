264b712e8a32aab28bc5b416a336bba2eba1f265
pushpaksinghal <pushpaksinghal1234@gmail.com>
2025-12-28 20:16:22 +0530

    [pushpak singhal]Add. practice the built in date time datetpe and formats

---
+++ core-csharp-practice/gcr-codebase/csharp-extra-string/csharp-date-time/DateFormatStylecs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.EXTRA.chsarp_date_time
+{
+    internal class DateFormatStylecs
+    {
+        static void Main(string[] args)
+        {
+            // inputing date
+            DateTime d = DateTime.Now;
+            //showing differnt format of showing date
+            Console.WriteLine(d.ToString("dd/MM/yyyy"));
+            Console.WriteLine(d.ToString("yyyy-MM-dd"));
+            Console.WriteLine(d.ToString("ddd, MMM dd, yyyy"));
+        }
+    }
+}
---
+++ core-csharp-practice/gcr-codebase/csharp-extra-string/csharp-date-time/MovingforwardInTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.EXTRA.chsarp_date_time
+{
+    internal class MovingforwardInTime
+    {
+        static void Main(string[] args)
+        {
+            //inputing the date 
+            DateTime curDate = Convert.ToDateTime(Console.ReadLine());
+            // moving teh date fowward in time by adding  
+            curDate = curDate.AddDays(7);
+            curDate = curDate.AddMonths(1);
+            curDate = curDate.AddYears(2);
+            curDate = curDate.AddDays(-21);
+            Console.WriteLine(curDate);
+        }
+    }
+}
