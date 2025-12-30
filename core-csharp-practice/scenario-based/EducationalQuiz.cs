cf77c1251d0b5e208fb396c2b3b45adf3be4cd0b
pushpaksinghal <pushpaksinghal1234@gmail.com>
2025-12-30 15:38:27 +0530

    [pushpak singhal] Add. practice senario based question

---
+++ core-csharp-practice/scenario-based/EducationalQuiz.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based
+{
+    internal class EducationalQuiz
+    {
+        static void Main(string[] args)
+        {
+            EducationalQuiz obj = new EducationalQuiz();
+            string[] correctAnswer = obj.Input();
+            obj.Display(correctAnswer);
+
+        }
+
+        string[] Input()
+        {
+            string[] corrrectAnswer = new string[10];
+            for(int i=0;i<corrrectAnswer.Length; i++)
+            {
+                corrrectAnswer[i] = Console.ReadLine();
+            }
+            return corrrectAnswer;
+        }
+
+        void Display(string[]corrcetAnswer)
+        {
+            while (true)
+            {
+                Console.WriteLine("Welcome to the quiz");
+                Console.WriteLine("1. Take the quiz.");
+                Console.WriteLine("2. Know your score");
+                Console.WriteLine("3. Feedback");
+                Console.WriteLine("3. Exit");
+                string[]userAnswer = new string[10];
+                int score = 0;
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        userAnswer = TakeQuiz();
+                        break;
+                    case 2:
+                        score = CalculateScore(userAnswer,corrcetAnswer);
+                        int per = (score / 10)*100;
+                        Console.WriteLine("you scored " + score + "/10");
+                        Console.WriteLine("your percantage " + per + "%");
+                        if (per < 33)
+                        {
+                            Console.WriteLine("fail");
+                        }
+                        else
+                        {
+                            Console.WriteLine("pass");
+                        }
+                            break;
+                    case 3:
+                        Feedback(userAnswer,corrcetAnswer);
+                        break;
+                    case 4:
+                        Console.WriteLine("exit");
+                        break;
+                    default:
+                        Console.WriteLine("invalid choice");
+                        break;
+                }
+                if (choice == 4)
+                {
+                    break;
+                }
+            }
+        }
+
+        string[] TakeQuiz()
+        {
+            string[] userAnswer = new string[10];
+
+            for (int i = 0; i < userAnswer.Length; i++)
+            {
+                Console.WriteLine("question" + i + 1);
+                userAnswer[i] = Console.ReadLine();
+            }
+            return userAnswer;
+        }
+
+        int CalculateScore(string[] userAnswer, string[]correctAnswer)
+        {
+            int score = 0;
+            for(int i=0;i<userAnswer.Length; i++)
+            {
+                if (correctAnswer[i].Equals(userAnswer[i].Trim().ToLower()))
+                {
+                    score++;
+                }
+                
+            }
+            return score;
+        }
+
+        void Feedback(string[] userAnswer, string[] correctAnswer)
+        {
+            int score = 0;
+            for (int i = 0; i < userAnswer.Length; i++)
+            {
+                if (correctAnswer[i].Equals(userAnswer[i].Trim().ToLower()))
+                {
+                    Console.WriteLine("your answer is correct. well done");
+                }
+                else
+                {
+                    Console.WriteLine("your answer was incorrect. try again next time");
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ core-csharp-practice/scenario-based/LibraryManagementSystem.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based
+{
+    internal class LibraryManagementSystem
+    {
+        
+        private static string passward = "Nowiamdeath";
+        static void Main(String[]args)
+        {
+            LibraryManagementSystem obj = new LibraryManagementSystem();
+            string[,] books = obj.InputBook();
+            obj.CheckAuth(books);
+        }
+        void CheckAuth(string[,]books)
+        {
+            
+            
+            while (true)
+            {
+                Console.WriteLine("WELCOME TO THEE LIBRARY");
+                Console.WriteLine("1. Admin Login");
+                Console.WriteLine("2. user Login");
+                Console.WriteLine("3. Exit");
+                int auth = Convert.ToInt32(Console.ReadLine());
+                switch (auth)
+                {
+                    case 1:
+                        string userEntry = Console.ReadLine();
+                        if (Passward(userEntry))
+                        {
+                            DisplayAdmin(books, books.GetLength(0));
+                            return;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You do not have admin status");
+                            break;
+                        }
+                    case 2:
+                        DisplayUser(books, books.GetLength(0));
+                        break;
+                    case 3:
+                        Console.WriteLine("Goodbye!");
+                        return;
+                }
+                if(auth == 3)
+                {
+                    break;
+                }
+            }   
+        }
+        private bool Passward(string userEntry)
+        {
+            
+            if (string.Equals(userEntry, passward))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        string[,] InputBook()
+        {
+            Console.WriteLine("renter the number of book to add in a library");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[,] books = new string[n, 4]; 
+            for(int i = 0; i < n; i++)
+            {
+                books[i, 0] = Console.ReadLine();//title
+                books[i, 1] = Console.ReadLine();//author
+                books[i, 2] = Console.ReadLine();//categories
+                books[i, 3] = Console.ReadLine();//status
+            }
+
+            return books;
+        }
+        void SeeBooks(String[,] books, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(books[i, 0]);
+            }
+        }
+
+        int Searching(string[,]books, int n)
+        {
+            string userSearch = Console.ReadLine();
+            int temp =-1 ;
+            userSearch = userSearch.Trim().ToLower();
+            for(int i = 0; i < n; i++)
+            {
+                if (books[i, 0].Equals(userSearch, StringComparison.OrdinalIgnoreCase) || books[i, 1].Equals(userSearch, StringComparison.OrdinalIgnoreCase))
+                {
+                    temp = i;
+                    if (string.Equals(books[i,3],"checked out"))
+                    {
+                        Console.WriteLine(books[i, 0] + "is present in the library but is checked out ");
+                    }
+                    Console.WriteLine(books[i, 0] + "is present in the library and is avalible ");
+                } 
+            }
+            return temp;
+        }
+
+        void RentingBook(string[,]books,int n,int index)
+        {
+            if (books[index, 3].Equals("check out", StringComparison.OrdinalIgnoreCase))
+            {
+                books[index, 3] = "checked out";
+                Console.WriteLine("You have rented the book" + books[index, 0]);
+            }
+            Console.WriteLine("You can not rent the book" + books[index, 0]);
+        }
+
+        private void Changepassward()
+        {
+            passward= Console.ReadLine();
+        }
+        private string[,] ResizeArray(string[,] oldArray, int newRows, int cols)
+        {
+            string[,] newArray = new string[newRows, cols];
+
+            int oldRows = oldArray.GetLength(0);
+
+            for (int i = 0; i < oldRows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    newArray[i, j] = oldArray[i, j];
+                }
+            }
+
+            return newArray;
+        }
+
+        private string[,] AddBook(String[,] books)
+        {
+            int rows = books.GetLength(0);
+            int cols = books.GetLength(1);
+            books = ResizeArray(books, rows+1,cols);
+
+            books[rows, 0] = Console.ReadLine(); ;
+            books[rows, 1] = Console.ReadLine(); ;
+            books[rows, 2] = Console.ReadLine(); ;
+            books[rows, 3] = Console.ReadLine(); ;
+
+            return books;
+        }
+
+        private string[,] RemoveBook(String[,] books,int index)
+        {
+            int rows = books.GetLength(0);
+            int cols = books.GetLength(1);
+            if (index < 0 || index >= rows)
+            {
+                Console.WriteLine("Invalid book index");
+                return books;
+            }
+            int lastRow = rows - 1;
+            books[index, 0] = books[lastRow, 0];
+            books[index, 1] = books[lastRow, 1];
+            books[index, 2] = books[lastRow, 2];
+            books[index, 3] = books[lastRow, 3];
+            books = ResizeArray(books, rows -1, cols);
+
+            return books;
+        }
+
+        void DisplayUser(string[,] books, int n)
+        {
+            while (true)
+            {
+                Console.WriteLine("WELCOME USER");
+                Console.WriteLine("1. See all the books");
+                Console.WriteLine("2. Search for a book (by title or author)");
+                Console.WriteLine("3. Rent the book ");
+                Console.WriteLine("4. exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                int indexOfSearchedBook = -1;
+                switch (choice)
+                {
+                    case 1:
+                        SeeBooks(books, n);
+                        continue;
+                    case 2:
+                        indexOfSearchedBook=Searching(books, n);
+                        continue;
+                    case 3:
+                        if(indexOfSearchedBook != -1)
+                        {
+                            RentingBook(books, n, indexOfSearchedBook);
+                        }
+                        else
+                        {
+                            Console.WriteLine("this book has already been checked out");
+                        }
+                        continue ;
+                    case 4:
+                        Console.WriteLine("Thanks for visiting.");
+                        break;
+                }
+                if(choice == 4)
+                {
+                    break;
+                }
+            }
+        }
+
+        void DisplayAdmin(String[,]books ,int n)
+        {
+            while (true)
+            {
+                Console.WriteLine("WELCOME ADMIN");
+                Console.WriteLine("1. See all the books");
+                Console.WriteLine("2. Search for a book (by title or author)");
+                Console.WriteLine("3. Change Passward");
+                Console.WriteLine("4. Add A Book");
+                Console.WriteLine("5. Remove a Book");
+                Console.WriteLine("6. exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                int indexOfSearchedBook = -1;
+                switch (choice)
+                {
+                    case 1:
+                        SeeBooks(books, n);
+                        continue;
+                    case 2:
+                        indexOfSearchedBook = Searching(books, n);
+                        continue;
+                    case 3:
+                        Changepassward();
+                        continue;
+                    case 4:
+                        books=AddBook(books);
+                        n = books.GetLength(0);
+                        continue;
+                    case 5:
+                        books =RemoveBook(books,indexOfSearchedBook);
+                        continue;
+                    case 6:
+                        Console.WriteLine("Done the days work");
+                        break;
+                    default:
+                        break;
+                }
+                
+
+            }
+
+        }
+
+    }
+}
