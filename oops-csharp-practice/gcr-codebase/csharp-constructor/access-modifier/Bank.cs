b551e75e2117a3d3cec426cb445f620950434795
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-02 22:15:26 +0530

    [pushpak singhal ]Add. practice the access modifier

---
+++ oops-csharp-practice/gcr-codebase/csharp-constructor/access-modifier/Bank.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.csharp_constructor_accessmodifiers.access_modifiers
+{
+    internal class Bank
+    {
+        static void Main(string[] args)
+        {
+            BankAccount a1 = new BankAccount();
+            BankAccount a2 = new BankAccount(102, "pushpak", 5000);
+            SavingsAccount s1 = new SavingsAccount();
+            a1.Display();
+            a2.Display();
+            s1.Display();
+        }
+    }
+
+    public class BankAccount
+    {
+        // creating atributes
+        public int accountNumber;
+        protected string accountHolder;
+        private double balance;
+        // consturctors
+        public BankAccount()
+        {
+            this.accountNumber = 101;
+            this.accountHolder = "Default User";
+            this.balance = 1000;
+        }
+        public BankAccount(int accountNumber, string accountHolder, double balance)
+        {
+            this.accountNumber = accountNumber;
+            this.accountHolder = accountHolder;
+            this.balance = balance;
+        }
+        // getter and setter 
+        public double GetBalance()
+        {
+            return balance;
+        }
+        public void SetBalance(double amount)
+        {
+            if (amount >= 0)
+            {
+                balance = amount;
+            }
+            else
+            {
+                Console.WriteLine("invalid bal");
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine("acc no. is " + accountNumber + " user name is " + accountHolder + " bal is " + GetBalance());
+        }
+    }
+    public class SavingsAccount() : BankAccount(103, "Suresh", 8000)
+    {
+        // ceraing a display method for child class
+        public void Display()
+        {
+            Console.WriteLine("savings acc no." + accountNumber + " user name is " + accountHolder + " bal is " + GetBalance());
+        }
+    }
+}
+
---
+++ oops-csharp-practice/gcr-codebase/csharp-constructor/access-modifier/Records.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.csharp_constructor_accessmodifiers.access_modifiers
+{
+    internal class Records
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            Employee e2 = new Employee(202, "IT", 45000);
+            Manager m1 = new Manager();
+            e1.Display();
+            e2.Display();
+            m1.Display();
+        }
+    }
+    // same as all the previous question
+    public class Employee
+    {
+        public int employeeID;
+        protected string department;
+        private double salary;
+        public Employee()
+        {
+            this.employeeID = 201;
+            this.department = "jhon";
+            this.salary = 30000;
+        }
+        public Employee(int employeeID, string department, double salary)
+        {
+            this.employeeID = employeeID;
+            this.department = department;
+            this.salary = salary;
+        }
+        public double GetSalary()
+        {
+            return salary;
+        }
+        public void SetSalary(double salary)
+        {
+            if (salary > 0)
+            {
+                this.salary = salary;
+            }
+            else
+            {
+                Console.WriteLine("invalid salary");
+            }
+        }
+        public void Display()
+        {
+            Console.WriteLine("employee id is " + employeeID + " dep is " + department + " salary is " + GetSalary());
+        }
+    }
+    public class Manager() : Employee(203, "Management", 70000)
+    {
+        // method for child class
+        public void Display()
+        {
+            Console.WriteLine("manager id is " + employeeID + " dep is " + department + " salary is " + GetSalary());
+        }
+    }
+}
