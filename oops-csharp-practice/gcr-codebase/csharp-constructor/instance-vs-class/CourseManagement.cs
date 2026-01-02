dd1d3fd75582872e2c592b082a207f09750573a2
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-02 15:42:30 +0530

    [pushpak singhal] Add. practiced the instance and class variable and methods type and implementation

---
+++ oops-csharp-practice/gcr-codebase/csharp-constructor/instance-vs-class/CourseManagement.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.csharp_constructor_accessmodifiers.Instance_vs_class
+{
+    internal class CourseManagement
+    {
+        //Atrbutes
+        static string university = "Madav unversity";
+        string courseName;
+        int duration;
+        double fee;
+        //constructor
+        public CourseManagement(string courseName,int duration, double fee)
+        {
+            this.courseName = courseName;
+            this.duration = duration;  
+            this.fee = fee;
+        }
+        //display method
+        public void DisplayCourseDetails()
+        {
+            Console.WriteLine("the name of the course is " + courseName + " the duration is " + duration + "for " + fee + "ruppe");
+        }
+        // updateing the name of the university
+        public static void UpdateInstituteName(string name)
+        {
+            university = name;
+            Console.WriteLine("the updated name of the university is " + university);
+        }
+
+    }
+
+    class Display()
+    {
+        static void Main(string[] args)
+        {
+            CourseManagement c1 = new CourseManagement("math", 4, 1000);
+            c1.DisplayCourseDetails();
+
+            CourseManagement c2 = new CourseManagement("science", 3, 3000);
+            c2.DisplayCourseDetails();
+
+            CourseManagement.UpdateInstituteName("GLA university");
+        }
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/csharp-constructor/instance-vs-class/ProductInventory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.csharp_constructor_accessmodifiers.Instance_vs_class
+{
+    internal class ProductInventory
+    {
+        //atributes 
+        string productName;
+        double price;
+        static int totalProduct;
+        //constructor
+        public ProductInventory(string productName, double price)
+        {
+            this.productName = productName;
+            this.price = price;
+            totalProduct++;
+        }
+        //display methods
+        public void DisplayProductDetails()
+        {
+            Console.WriteLine("the product name is " + productName + "and the price is " + price);
+        }
+        //udateing the fee
+        static public void DisplayTotalProducts()
+        {
+            Console.WriteLine("total product are " + totalProduct);
+        }
+
+
+    }
+
+    class Displat()
+    {
+        static void Main(string [] args)
+        {
+            ProductInventory p1 = new ProductInventory("milk", 10.2);
+            p1.DisplayProductDetails();
+
+            ProductInventory p2 = new ProductInventory("oreo", 21.1);
+            p2.DisplayProductDetails();
+
+            ProductInventory.DisplayTotalProducts();
+
+
+        }
+    }
+}
