ba9b1f98627ce261474523ba75cae0c30615957c
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-02 11:33:19 +0530

    [pushpak singhal] Add. Senario based question on object and classes

---
+++ oops-csharp-practice/senario-based-codebase/Bus-Dirves/EnterUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based.Bus_Dirves
+{
+    internal class EnterUser
+    {
+        static void Main(string[] args)
+        {
+            //creating an object for the clas bus
+            Bus obj = new Bus();
+            obj.Menu();
+        }
+    }
+}
---
+++ oops-csharp-practice/senario-based-codebase/Mela-Game/Entry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based.Mela_Game
+{
+    internal class Entry
+    {
+        static void Main(String[] args)
+        {
+            // creating the obj of the mela class in the main  method
+            StrawGame obj = new StrawGame();
+            obj.Menu();
+        }
+    }
+}
