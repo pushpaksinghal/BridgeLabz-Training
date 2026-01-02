d64e907b6fdb510f28d890c1344b01d474ad3581
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-02 22:56:22 +0530

    [pushpak singhal]Add. practice question for the keywords like this sealed readonly and is operator

---
+++ oops-csharp-practice/gcr-codebase/csharp-keywords-and-operaotr/level-1/Office.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.keywords_and_operator.level_1
+{
+    internal class Office
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee("pushpak", 001, "Dev");
+            //using the is opretor
+            if (e1 is Employee)
+            {
+                e1.Display();
+            }
+
+            Employee.DisplayTotalEmployees();
+        }
+    }
+
+    public class Employee
+    {
+        //static atributes
+        public static string CompanyName = "BridgeLabz";
+        static int totalEmployees = 0;
+        // using thr readonly keyword
+        public readonly int Id;
+        string Name;
+        string Designation;
+
+        public Employee(string Name, int Id, string Designation)
+        {
+            this.Name = Name;
+            this.Id = Id;
+            this.Designation = Designation;
+            totalEmployees++;
+        }
+
+        public static void DisplayTotalEmployees()
+        {
+            Console.WriteLine(totalEmployees);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Name + " " + Id + " " + Designation);
+        }
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/csharp-keywords-and-operaotr/level-1/Rto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.keywords_and_operator.level_1
+{
+    internal class Rto
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v1 = new Vehicle("pushpak", "car", 1411);
+            // is operator if true then display 
+            if (v1 is Vehicle)
+            {
+                v1.Display();
+            }
+            // updating the fee
+            Vehicle.UpdateRegistrationFee(2000);
+        }
+    }
+
+    public class Vehicle
+    {
+        public static int RegistrationFee = 1500;
+        // readonly variable
+        public readonly int RegistrationNumber;
+
+        string OwnerName;
+        string VehicleType;
+        // parameter con
+        public Vehicle(string OwnerName, string VehicleType, int RegistrationNumber)
+        {
+            // this keyword
+            this.OwnerName = OwnerName;
+            this.VehicleType = VehicleType;
+            this.RegistrationNumber = RegistrationNumber;
+        }
+
+        public static void UpdateRegistrationFee(int fee)
+        {
+            RegistrationFee = fee;
+            Console.WriteLine(RegistrationFee);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(OwnerName + " " + VehicleType + " " + RegistrationNumber);
+        }
+    }
+}
