8ecf514e5c889bfffd0d0c01f457410c2dd47f4d
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-03 16:55:22 +0530

    [pushpak singhal] Add. practiced question on class modeling

---
+++ oops-csharp-practice/gcr-codebase/csharp-modeling-and-diagram/chsarp-modeling/SchoolSystem.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BridgelabzTraining.class_modeling_and_diagram.csharp_modeling
+{
+    internal class SchoolSystem
+    {
+        // Main method
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("pushapk");
+            Course c1 = new Course("english");
+            // Enroll student in course
+            s1.Enroll(c1);
+            s1.ViewCourses();
+            c1.ViewStudents();
+        }
+    }
+
+    class Student
+    {
+        // Student name
+        public string Name;
+        public Course[] Courses = new Course[5];
+        int i = 0;
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+        // Enroll in a course
+        public void Enroll(Course course)
+        {
+            Courses[i++] = course;
+            course.AddStudent(this);
+        }
+        // View enrolled courses
+        public void ViewCourses()
+        {
+            Console.WriteLine(Name + " enrolled courses:");
+            for (int j = 0; j < i; j++)
+                Console.WriteLine(Courses[j].Name);
+        }
+    }
+
+    class Course
+    {
+        public string Name;
+        Student[] Students = new Student[5];
+        int i = 0;
+
+        public Course(string name)
+        {
+            Name = name;
+        }
+        // Add student to the course
+        public void AddStudent(Student student)
+        {
+            Students[i++] = student;
+        }
+        // View enrolled students
+        public void ViewStudents()
+        {
+            Console.WriteLine("Students in " + Name);
+            for (int j = 0; j < i; j++)
+                Console.WriteLine(Students[j].Name);
+        }
+    }
+}
+
---
+++ oops-csharp-practice/gcr-codebase/csharp-modeling-and-diagram/chsarp-modeling/UniversityManagement.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BridgelabzTraining.class_modeling_and_diagram.csharp_modeling
+{
+    internal class UniversityManagement
+    {
+        static void Main(string[] args)
+        {
+            // Create instances of Course, Student, and Professor
+            Course c = new Course("AI");
+            Student s = new Student("Neha");
+            Professor p = new Professor("Dr. Rao");
+            // Enroll student in course and assign professor to course
+            s.EnrollCourse(c);
+            p.AssignProfessor(c);
+        }
+    }
+
+    class Student
+    {
+        public string Name;
+        public Student(string name)
+        {
+            Name = name;
+        }
+        // Method to enroll in a course
+        public void EnrollCourse(Course course)
+        {
+            Console.WriteLine(Name + " enrolled in " + course.Name);
+        }
+    }
+
+    class Professor
+    {
+        public string Name;
+        public Professor(string name)
+        {
+            Name = name;
+        }
+        // Method to assign professor to a course
+        public void AssignProfessor(Course course)
+        {
+            course.ProfessorName = Name;
+            Console.WriteLine(Name + " assigned to " + course.Name);
+        }
+    }
+
+    class Course
+    {
+        public string Name;
+        public string ProfessorName;
+        // Constructor
+        public Course(string name)
+        {
+            Name = name;
+        }
+    }
+}
+
