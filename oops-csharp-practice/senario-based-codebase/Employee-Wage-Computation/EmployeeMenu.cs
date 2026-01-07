bf64a34b4abede0df816d680351c2b0591ff7ec2
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-07 15:16:37 +0530

    [pushpak singhal] refactor. updated teh menu file with switch case for version 4.

--- oops-csharp-practice/senario-based-codebase/Employee-Wage-Computation/EmployeeMenu.cs
+++ oops-csharp-practice/senario-based-codebase/Employee-Wage-Computation/EmployeeMenu.cs
@@ -1,79 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BridgelabzTraining.senario_based.Employee_Wage_Computation
 {
     sealed class EmployeeMenu
     {
         private IEmployee employee;
+        // refactored the file with switch case instead of if else for varsion 4
         public void TypeEmployee()
         {
             bool flag = true;
-
             while (flag)
             {
                 Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
                 Console.WriteLine("1. Full Time Employee");
                 Console.WriteLine("2. Part Time Employee");
-                Console.WriteLine("3. Ext");
+                Console.WriteLine("3. Exit");
 
                 int which = Convert.ToInt32(Console.ReadLine());
-                if(which == 1)
+                switch (which)
                 {
-                    EmployeeOption();
-                    break;
-                }
-                else if (which == 2)
-                {
-                    PartEmployeeOption();
-                }
-                else if(which == 3)
-                {
-                    flag = false;
-                    break;
+                    case 1:
+                        EmployeeOption();
+                        break;
+                    case 2:
+                        PartEmployeeOption();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
                 }
             }
         }
+
         public void EmployeeOption()
         {
             bool flag = true;
             employee = new EmployeeUtitlityImp();
-            Employee emp1 =null;
+            Employee emp1 = null;
             while (flag)
             {
                 Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
-                Console.WriteLine("1.Add Employee");
-                Console.WriteLine("2.Check Employee Attendance");
-                Console.WriteLine("3. Check Employee Daily Wages"); // changes  for version 2
-                Console.WriteLine("4.Exit");
+                Console.WriteLine("1. Add Employee");
+                Console.WriteLine("2. Check Employee Attendance");
+                Console.WriteLine("3. Check Employee Daily Wages");
+                Console.WriteLine("4. Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
-                if(choice == 1)
+
+                switch (choice)
                 {
-                    emp1 = employee.AddEmployee();
-                    Console.WriteLine(emp1);
-                    Console.WriteLine("Employee added.");
-                }
-                else if(choice == 2)
-                {
-                    employee.Attendance(emp1.GetId());
-                }
-                //for version 2 to print daily wages of an employee
-                else if(choice == 3)
-                {
-                    employee.DailyWage(emp1.GetId());
-                }
-                else if (choice == 4)
-                {
-                    Console.WriteLine("Thnaks for visiting");
-                    flag = false;
-                    break;
+                    case 1:
+                        emp1 = employee.AddEmployee();
+                        Console.WriteLine(emp1);
+                        Console.WriteLine("Employee added.");
+                        break;
+                    case 2:
+                        // added an option to add employee first so that id can be taken
+                        if (emp1 != null)
+                            employee.Attendance(emp1.GetId());
+                        else
+                            Console.WriteLine("Please add employee first.");
+                        break;
+                    case 3:
+                        if (emp1 != null)
+                            employee.DailyWage(emp1.GetId());
+                        else
+                            Console.WriteLine("Please add employee first.");
+                        break;
+                    case 4:
+                        Console.WriteLine("Thanks for visiting");
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
                 }
             }
         }
-        //added part time employee options 
         public void PartEmployeeOption()
         {
             bool flag = true;
@@ -81,27 +86,32 @@ namespace BridgelabzTraining.senario_based.Employee_Wage_Computation
             Employee emp1 = null;
             while (flag)
             {
-                Console.WriteLine("Choose ");
-                Console.WriteLine("1.Add Employee");
-                Console.WriteLine("2.Check Employee Daily Wages"); // changes  for version 3
-                Console.WriteLine("3.Exit");
+                Console.WriteLine("Choose");
+                Console.WriteLine("1. Add Employee");
+                Console.WriteLine("2. Check Employee Daily Wages");
+                Console.WriteLine("3. Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
-                if (choice == 1)
+
+                switch (choice)
                 {
-                    emp1 = employee.AddEmployee();
-                    Console.WriteLine(emp1);
-                    Console.WriteLine("Employee added.");
-                }
-                //for version 3 to print daily wages of an part employee
-                else if (choice == 2)
-                {
-                    employee.DailyWage(emp1.GetId());
-                }
-                else if (choice == 3)
-                {
-                    Console.WriteLine("Thnaks for visiting");
-                    flag = false;
-                    break;
+                    case 1:
+                        emp1 = employee.AddEmployee();
+                        Console.WriteLine(emp1);
+                        Console.WriteLine("Employee added.");
+                        break;
+                    case 2:
+                        if (emp1 != null)
+                            employee.DailyWage(emp1.GetId());
+                        else
+                            Console.WriteLine("Please add employee first.");
+                        break;
+                    case 3:
+                        Console.WriteLine("Thanks for visiting");
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
                 }
             }
         }
