704245ba6c2cb1479ca33cb5a6c2133e94b2249d
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-13 15:17:09 +0530

    [pushpak singhal ] Add. practice senario based question

---
+++ oops-csharp-practice/senario-based-codebase/User-Personal-Bookshelf/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based.User_Personal_Bookshelf
+{
+    internal class Book
+    {
+        private string title;
+        private string author;
+
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+        public string Title() { return title; }
+        public string Author() { return author; }
+
+        public override string ToString()
+        {
+            return "Title: " + title + "\nAuthor: " + author;
+        }
+    }
+}
