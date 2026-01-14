c23c0114c10aed3415560f6ca37fed3387d4d24e
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-14 11:51:59 +0530

    [pushpak singhal] ReFactor. added ability to create contacts in addressbook UC-1

---
+++ oops-csharp-practice/senario-based-codebase/AddressBook/AddressBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based.AddressBook
+{
+    internal class AddressBook
+    {
+        private UserContact[] contacts;
+        private int contactcount;
+
+        public AddressBook(UserContact[]contacts,int contactcount)
+        {
+            this.contacts = contacts;
+            this.contactcount = contactcount;
+        }
+
+        public UserContact[] Contacts() { return contacts; }
+        public int ContactCount() { return contactcount; }
+
+
+    }
+}
