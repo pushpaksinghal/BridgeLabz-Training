b66b18914027219dbf36103b7469f908a9cbfdbd
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-16 14:57:14 +0530

    [pushpak singhal] refactor . used two arrays in utility

--- oops-csharp-practice/senario-based-codebase/DynamicBookShelf/ShwlfUtility.cs
+++ oops-csharp-practice/senario-based-codebase/DynamicBookShelf/ShwlfUtility.cs
@@ -1,84 +1,105 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BridgelabzTraining.senario_based.DynamicBookShelf
 {
-    internal class ShwlfUtility: IBooks
+    internal class ShwlfUtility : IBooks
     {
-        Dictionary<string,ShelfBooks>library = new Dictionary<string,ShelfBooks>();
+        private const int MAX_GENRES = 20;
+
+        private string[] genres = new string[MAX_GENRES];
+        private ShelfBooks[] shelves = new ShelfBooks[MAX_GENRES];
+        private int genreCount = 0;
+        // helper function for finding the genre
+        private int FindGenreIndex(string genre)
+        {
+            genre = genre.ToLower();
+            for (int i = 0; i < genreCount; i++)
+            {
+                if (genres[i] == genre)
+                    return i;
+            }
+            return -1;
+        }
 
         public void AddBook()
         {
-            String name = Console.ReadLine();
-            String author = Console.ReadLine();
-            String genre = Console.ReadLine();
-            bool status = true; ;
+            string name = Console.ReadLine();
+            string author = Console.ReadLine();
+            string genre = Console.ReadLine().ToLower();
+            bool status = true;
 
             Book book = new Book(name, author, genre, status);
-            if (library.ContainsKey(genre.ToLower())) 
+
+            int index = FindGenreIndex(genre);
+
+            if (index != -1)
             {
-                library[genre.ToLower()].Add(book);
+                shelves[index].Add(book);
             }
             else
             {
-                library[genre.ToLower()] = new ShelfBooks();
-                library[genre.ToLower()].Add(book);
+                if (genreCount >= MAX_GENRES)
+                {
+                    Console.WriteLine("Genre limit reached.");
+                    return;
+                }
+
+                genres[genreCount] = genre;
+                shelves[genreCount] = new ShelfBooks();
+                shelves[genreCount].Add(book);
+                genreCount++;
             }
 
-            Console.WriteLine("Book Added to the perticular genre shelf");
+            Console.WriteLine("Book added to the particular genre shelf.");
         }
 
         public void DeleteBook()
         {
-            string name = Console.ReadLine(); 
-            string genre = Console.ReadLine();
-            if (library.ContainsKey(genre.ToLower()))
+            string name = Console.ReadLine();
+            string genre = Console.ReadLine().ToLower();
+
+            int index = FindGenreIndex(genre);
+
+            if (index != -1)
             {
-                bool deleted = library[genre.ToLower()].Remove(name);
-                if (deleted)
-                {
-                    Console.WriteLine("Book Deleted.");
-                }
-                else
-                {
-                    Console.WriteLine("no bbook with this name");
-                }
+                bool deleted = shelves[index].Remove(name);
+                Console.WriteLine(deleted ? "Book deleted." : "No book with this name.");
             }
             else
             {
-                Console.WriteLine("there is no book  to delete");
+                Console.WriteLine("This genre does not exist.");
             }
         }
 
         public void DisplayByGenre()
         {
-            string genre = Console.ReadLine();
-            if (library.ContainsKey(genre.ToLower()))
+            string genre = Console.ReadLine().ToLower();
+            int index = FindGenreIndex(genre);
+
+            if (index != -1)
             {
-                library[genre.ToLower()].Display();
-                Console.WriteLine();
+                shelves[index].Display();
             }
             else
             {
-                Console.WriteLine("Add this genrebooks to display all the books");
+                Console.WriteLine("No books found for this genre.");
             }
         }
 
         public void UpdateStatus()
         {
-            string genre = Console.ReadLine();
+            string genre = Console.ReadLine().ToLower();
             string name = Console.ReadLine();
-            if (library.ContainsKey(genre.ToLower()))
+
+            int index = FindGenreIndex(genre);
+
+            if (index != -1)
             {
-                library[genre.ToLower()].Update(name);
-                return;
+                shelves[index].Update(name);
             }
             else
             {
-                Console.WriteLine("This genre des not exist");
+                Console.WriteLine("This genre does not exist.");
             }
         }
     }
