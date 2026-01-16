48947d30d6e53931bdf5ba5b88488e54fe7c5c86
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-16 14:22:08 +0530

    [pushpak singhal]Add . practice a book shelf but genre wise

---
+++ oops-csharp-practice/senario-based-codebase/DynamicBookShelf/IBooks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based.DynamicBookShelf
+{
+    internal interface IBooks
+    {
+        void AddBook();
+        void DeleteBook();
+        void DisplayByGenre();
+        void UpdateStatus();
+    }
+}
---
+++ oops-csharp-practice/senario-based-codebase/DynamicBookShelf/ShwlfUtility.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based.DynamicBookShelf
+{
+    internal class ShwlfUtility: IBooks
+    {
+        Dictionary<string,ShelfBooks>library = new Dictionary<string,ShelfBooks>();
+
+        public void AddBook()
+        {
+            String name = Console.ReadLine();
+            String author = Console.ReadLine();
+            String genre = Console.ReadLine();
+            bool status = true; ;
+
+            Book book = new Book(name, author, genre, status);
+            if (library.ContainsKey(genre.ToLower())) 
+            {
+                library[genre.ToLower()].Add(book);
+            }
+            else
+            {
+                library[genre.ToLower()] = new ShelfBooks();
+                library[genre.ToLower()].Add(book);
+            }
+
+            Console.WriteLine("Book Added to the perticular genre shelf");
+        }
+
+        public void DeleteBook()
+        {
+            string name = Console.ReadLine(); 
+            string genre = Console.ReadLine();
+            if (library.ContainsKey(genre.ToLower()))
+            {
+                bool deleted = library[genre.ToLower()].Remove(name);
+                if (deleted)
+                {
+                    Console.WriteLine("Book Deleted.");
+                }
+                else
+                {
+                    Console.WriteLine("no bbook with this name");
+                }
+            }
+            else
+            {
+                Console.WriteLine("there is no book  to delete");
+            }
+        }
+
+        public void DisplayByGenre()
+        {
+            string genre = Console.ReadLine();
+            if (library.ContainsKey(genre.ToLower()))
+            {
+                library[genre.ToLower()].Display();
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Add this genrebooks to display all the books");
+            }
+        }
+
+        public void UpdateStatus()
+        {
+            string genre = Console.ReadLine();
+            string name = Console.ReadLine();
+            if (library.ContainsKey(genre.ToLower()))
+            {
+                library[genre.ToLower()].Update(name);
+                return;
+            }
+            else
+            {
+                Console.WriteLine("This genre des not exist");
+            }
+        }
+    }
+}
