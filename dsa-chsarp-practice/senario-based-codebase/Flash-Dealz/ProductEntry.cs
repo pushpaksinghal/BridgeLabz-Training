17040601bf1651a7bca2f8a88c223c6e12df90e2
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-17 15:40:41 +0530

    [pushpak singhal] Add . practice implementation ofa  linkedlist in a oops senario based queestion to understand real world problem

---
+++ dsa-chsarp-practice/senario-based-codebase/Flash-Dealz/ProductEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based.Flash_Dealz
+{
+    internal class ProductEntry
+    {
+        public static void Main(String[] args)
+        {
+            // made the object of the menu class and called the menu method
+            ProductMenu menu = new ProductMenu();
+            menu.Menu();
+        }
+    }
+}
---
+++ dsa-chsarp-practice/senario-based-codebase/Flash-Dealz/ProductUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BridgelabzTraining.senario_based.Flash_Dealz
+{
+    internal class ProductUtility:IProduct
+    {
+        //uisng the linked list to make the add and bestdeal method and hiding the implementation by using interface
+        ProductLinkedList productLinkedList = new ProductLinkedList();
+        int count = 0;
+        public void AddProduct()
+        {
+            int id = count++;
+            string name = Console.ReadLine();
+            double discount = Convert.ToDouble(Console.ReadLine()); 
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            productLinkedList.Add(new Product(id, name, discount, price));
+
+            Console.WriteLine("The Product has been added");
+        }
+
+        public void GetBestDeal()
+        {
+            ProductNode head = productLinkedList.GetHead();
+            head = productLinkedList.QuickSort(head);
+
+            Console.WriteLine("The most discount is on " + head.product.GetName() + " of " + head.product.GetDiscount() + " and the discounted price is " + head.product.GetDiscountPrice());
+        }
+    }
+}
