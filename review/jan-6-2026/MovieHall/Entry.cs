2ab761534bb6b7a8e7f034e2b167d183177664c8
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-19 10:51:27 +0530

    [pushpak singhal] Add. Review Question on date 6 jan 2026

---
+++ review/jan-6-2026/MovieHall/Entry.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace BridgelabzTraining.review.MovieHall
+//{
+//    internal class entry
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            menu n = new menu();
+//            n.start();
+//        }
+//    }
+//}
---
+++ review/jan-6-2026/MovieHall/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.review.MovieHall
+{
+    internal class Movie
+    {
+        public string name;
+        public int length;
+        public int numberOfTicket;
+        public double price;
+
+        public Movie(string name, int length, int numberOfTicket,double price)
+        {
+            this.name = name;
+            this.length = length;
+            this.numberOfTicket = numberOfTicket;  
+            this.price = price;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Movie name " + name);
+            Console.WriteLine("length " + length);
+            Console.WriteLine("Tickets Left " + numberOfTicket);
+            Console.WriteLine("Price " + price);
+        }
+    }
+}
