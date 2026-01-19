c2eafd27cc717084d0265e21b822a6fcab770ca0
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-19 15:31:06 +0530

    [pushpak singhal] Add. review of dsa and oops concepts with sorting

---
+++ review/jan-19-2026/Hospital-ManageMent-System/Ihospital.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review_3.Hospital_ManageMent_System
+{
+    internal interface Ihospital
+    {
+        void AddPaitet();
+
+        void InRemovePaitent(int index);
+        void OutRemovePaitent(int index);
+
+        void InBillGenerate(int index);
+        void OutBillGenerate(int index);
+
+        void InSort();
+        void OutSort();
+
+        void InDisplay();
+        void OutDisplay();
+    }
+}
+
---
+++ review/jan-19-2026/Hospital-ManageMent-System/OutPaitent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Review_3.Hospital_ManageMent_System
+{
+    internal class OutPaitent : Paitent
+    {
+        private int hours;
+        private int roomId;
+
+        public OutPaitent(string name, string illness, int id, int hours, int roomId)
+            : base(name, illness, id)
+        {
+            this.hours = hours;
+            this.roomId = roomId;
+        }
+
+        public override double CalculateBill()
+        {
+            return hours * 100;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() +
+                   $"\nRoom Id: {roomId}\nHours Visited: {hours}\nBill: {CalculateBill()}";
+        }
+    }
+}
+
