6ad7d9c42e5477c12fb92f8d1876bba9dbe24cf9
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-20 10:52:00 +0530

    [pushpak singhal]Add. added a practice senario based question

---
+++ dsa-chsarp-practice/senario-based-codebase/ExamProctor/ExamMenu.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamProctor
+{
+    sealed class ExamMenu
+    {
+        private IExamService service = new ExamServiceUtilityImpl();
+
+        public void Start()
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Add Student");
+                Console.WriteLine("2. Visit Question");
+                Console.WriteLine("3. Answer Question");
+                Console.WriteLine("4. Submit Exam");
+                Console.WriteLine("5. Exit");
+
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1: 
+                        service.AddStudent(); 
+                        break;
+
+                    case 2:
+                        Console.Write("Question ID: ");
+                        service.VisitQuestion(int.Parse(Console.ReadLine()));
+                        break;
+
+                    case 3:
+                        Console.Write("Question ID: ");
+                        int q = int.Parse(Console.ReadLine());
+                        Console.Write("Answer: ");
+                        service.AnswerQuestion(q, Console.ReadLine());
+                        break;
+
+                    case 4: 
+                        service.SubmitExam(); 
+                        break;
+
+                    case 5: 
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ dsa-chsarp-practice/senario-based-codebase/ExamProctor/IExamService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamProctor
+{
+    interface IExamService
+    {
+        void AddStudent();
+        void VisitQuestion(int qId);
+        void AnswerQuestion(int qId, string answer);
+        void SubmitExam();
+    }
+}
