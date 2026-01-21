52ccbf8b98e79d5117d8b397f3a2a88d578fdd7f
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-01-21 15:18:33 +0530

    [pushpak singhal]Add. Practiced collection in a sinario based quesstion

---
+++ collections-csharp-practice/senario-based-codebase/CustomerQueueMenagement/CustomerQueueMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based.CustomerQueueMenagement
+{
+    internal class CustomerQueueMain
+    {
+        public static void Main(String[] args)
+        {
+            MarketMenu m = new MarketMenu();
+            m.Start();
+        }
+    }
+}
---
+++ collections-csharp-practice/senario-based-codebase/CustomerQueueMenagement/Items.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based.CustomerQueueMenagement
+{
+    internal class Items
+    {
+        protected string name;
+        protected double price;
+        protected int stack;
+
+        public Items(string name, double price, int stack)
+        {
+            this.name = name;
+            this.price = price;
+            this.stack = stack; 
+        }
+
+        public string GetName() { return name; }
+        public double GetPrice() { return price; }
+        public int GetStock() { return stack; }
+
+        public void ReduceStock(int qty)
+        {
+            stack -= qty;
+        }
+        public override string ToString()
+        {
+            return "Name: " + name+"\nPrice per piece: "+price;
+        }
+    }
+}
