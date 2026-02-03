3e8487aa358ae265a9dd8abc6882dfbf761e550c
pushpaksinghal <pushpaksinghal1234@gmail.com>
2026-02-03 11:09:37 +0530

    [pushpak singhal ]Add. a practice senario based question Flip Key Logical

---
+++ collections-csharp-practice/senario-based-codebase/Flip-Key-Logical.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgelabzTraining.senario_based
+{
+    internal class Flip_Key_Logical
+    {
+        public static void Main(String[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1. Convert Your String.");
+                Console.WriteLine("2. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1: CleanseAndInvert(); break;
+                    case 2: flag = false; break;
+                    default: Console.WriteLine("Invaild choice."); flag = false; break;
+                }
+            }
+        }
+
+        static void CleanseAndInvert()
+        {
+            Console.WriteLine("Enter  a String. ");
+            string input = Console.ReadLine();
+
+            if(input.Length<6 && input.Contains(@"[ \W]"))
+            {
+                Console.WriteLine("Invalid String Input");
+                return;
+            }
+            else
+            {
+                int count = 0;
+                string temp = "";
+                input = input.ToLower();
+                for(int i = input.Length - 1; i >= 0; i--)
+                {
+                    if ((int)input[i] %2 != 0)
+                    {
+                        if (count % 2 == 0)
+                        {
+                            temp += input[i].ToString().ToUpper();
+                        }
+                        else
+                        {
+                            temp += input[i];
+                        }
+                        count++;
+                    }
+                }
+                Console.WriteLine("The generated key is - " + temp);
+                return;
+            }
+        }
+    }
+}
